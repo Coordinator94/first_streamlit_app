5c95b9f59ac47401d6dc3a0238a4a7700e3e9bd1
Deacon <Coordinator94@users.noreply.github.com>
2022-05-06 10:17:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
-import stremlit
+import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom\'s New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kalel, Spinish & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
