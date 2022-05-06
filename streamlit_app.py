0dd9f1519928c2487970979dd3cb957cfc50df7b
Deacon <Coordinator94@users.noreply.github.com>
2022-05-06 12:00:21 -0400

    Update streamlit_app.py
    Picklist fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,6 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-#Addition of Pick list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 streamlit.title('My Mom\'s New Healthy Diner')
 
