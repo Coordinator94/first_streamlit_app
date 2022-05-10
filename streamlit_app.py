70e629a163aaf5c20c2f302bfaea483abe1c1cf1
Deacon <Coordinator94@users.noreply.github.com>
2022-05-10 08:09:42 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ streamlit.dataframe(fruits_to_show)
 
 
 # Function for Fruitvice API Responses
-Def get_fruityvice_data(this_fruit_choice)
-     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+Def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
